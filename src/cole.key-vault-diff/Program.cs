7651856124a21fa51c75598b7d545f8108cace2c
Mike Cole <cole.mike@gmail.com>
2024-01-17 18:21:45 -0600

    Cleanup

--- src/cole.key-vault-diff/Program.cs
+++ src/cole.key-vault-diff/Program.cs
@@ -1,15 +1,4 @@
-﻿//Setup DI for SecretDiffEngine
-//Setup tests for console tests
-//Test for: mocked operations show proper sign
-//  mocked key names space correctly on console
-//Future:
-//  Test operation to view contents of modified values, with confirmation
-//  Test to actually make the changes
-//Bugs:
-// Secret clients returns certs as well? strange.
-
-using System.CommandLine.Builder;
-using cole.key_vault_diff;
+﻿using cole.key_vault_diff;
 using cole.key_vault_diff.Secret;
 using DotMake.CommandLine;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,9 +7,5 @@ Cli.Ext.ConfigureServices(services =>
 {
     services.AddTransient<ISecretDiffEngine, SecretDiffEngine>();
 });
-        
-//Do I need this extra dependency injection stuff?
-await Cli.RunAsync<RootCommand>(args, builder => builder.UseDefaults().UseDependencyInjection(services =>
-{
-    services.AddSingleton<ISecretDiffEngine, SecretDiffEngine>();
-}));
+
+await Cli.RunAsync<RootCommand>();
